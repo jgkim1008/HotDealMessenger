85c4edd439584e0c16131bdbacc13cad5b47d01e
jgkim1008 <rlawnsrjs100@naver.com>
2024-01-07 01:07:19 +0900

    Initial Commit

---
+++ HotDealMessenger/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HotDealMessenger
+//
+//  Created by 김준건 on 2024/01/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HotDealMessenger/HotDealMessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotDealMessengerApp.swift
+//  HotDealMessenger
+//
+//  Created by 김준건 on 2024/01/07.
+//
+
+import SwiftUI
+
+@main
+struct HotDealMessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
