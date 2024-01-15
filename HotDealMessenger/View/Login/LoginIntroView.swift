469450d54648b7b6eba3fd4094130d2e725721dc
jgkim1008 <rlawnsrjs100@naver.com>
2024-01-16 00:30:18 +0900

    feat: LoginIntroView 구현
    - LoginIntroView 구현 및 LoginView로 NavigationDestination 설정
    - NavigationStack 사용을 위한 ios16 버전 셋팅
    - LoginButtonStyle Custom 생성

--- HotDealMessenger/View/Login/LoginIntroView.swift
+++ HotDealMessenger/View/Login/LoginIntroView.swift
@@ -8,8 +8,35 @@
 import SwiftUI
 
 struct LoginIntroView: View {
+    @State private var isPresentedLoginView: Bool = false
     var body: some View {
-        Text("LoginView!")
+        NavigationStack {
+            VStack(spacing: 20) {
+                Spacer()
+                
+                Text("환영합니다.")
+                    .font(.system(size:26, weight: .bold))
+                    .foregroundColor(.black)
+                Text("매일 핫딜 관련된 정보를 부담없이 받아보세요!")
+                    .font(.system(size: 12))
+                    .foregroundColor(.grayDeep)
+                
+                Spacer()
+                Button {
+                    isPresentedLoginView.toggle()
+                } label: {
+                    Text("로그인")
+                        .font(.system(size:14))
+                        .foregroundColor(.bkText)
+                        .frame(maxWidth: .infinity, maxHeight: 40)
+                }.buttonStyle(LoginButtonStyle(textColor: .lineAppColor))
+            }
+            .navigationDestination(isPresented: $isPresentedLoginView) { 
+                LoginView()
+            }
+            
+     
+        }
     }
 }
 
